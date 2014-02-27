6d9c65c59f2598c14bd1a8778c3d3500b8b1deee
Patrick Shampine <pshampine@gunnjerkens.com>
2014-02-26 17:58:05 -0800

    Added hostname statement around browser sync

--- public/inc/footer.php
+++ public/inc/footer.php
@@ -1,9 +1,10 @@
     <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
-    <script src="js/main.js"></script>
-    <!-- Remove before production -->
-    <script src='http://localhost:3000/socket.io/socket.io.js'></script>
-    <script src='http://localhost:3001/browser-sync-client.min.js'></script>
-    <!-- Will hack out a solution later -->
-  </body>
+    <script src="js/main.js"></script><?php
 
+    if ($environment === 'dev') {
+     echo "<script src='http://localhost:3000/socket.io/socket.io.js'></script>";
+     echo "<script src='http://localhost:3001/browser-sync-client.min.js'></script>";
+    } ?>
+
+  </body>
 </html>
