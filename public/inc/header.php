fab15f5e82a06324850d592e03bf5ed8e2f80ae6
Patrick Shampine <pshampine@gunnjerkens.com>
2014-03-18 16:34:40 -0700

    Meta tag fix, respond.js ordering fix, rm robots

--- public/inc/header.php
+++ public/inc/header.php
@@ -4,14 +4,15 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="viewport" content="width=device-width" initial-scale="1.0" />
-
+    <meta name="viewport" content="width=device-width" initial-scale="1.0" maximum-scale="1.0" user-scalable="no" /><?php 
+    if ($environment === 'production') { echo '<meta name="robots" content="index, follow">'; } 
+    else { echo '<meta name="robots" content="noindex, nofollow">'; } ?>
     <title><?php echo theTitle(); ?></title>
     <meta name="description" content="<?php echo theDescription(); ?>">
-    <script type='text/javascript' src='js/modernizr.min.js'></script>
-    <script type='text/javascript' src='js/respond.min.js'></script>
     <link rel="stylesheet" href="style/screen.css">
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <script type='text/javascript' src='js/modernizr.min.js'></script>
+    <script type='text/javascript' src='js/respond.min.js'></script>
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
     <script>
       (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
