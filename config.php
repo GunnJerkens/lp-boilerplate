56e2a7de08180ed12d35c31505c7633b4c8d18be
Patrick Shampine <pshampine@gunnjerkens.com>
2014-04-11 11:56:01 -0700

    Bugfix -- GA hostname wasn't dropping http causing inproper tracking

--- config.php
+++ config.php
@@ -69,4 +69,23 @@ function theDescription() {
   }
 }
 
+function theAnalytics() {
+  global $environment, $hostname, $google_analytics;
+  if($environment === 'production') {
+    $hostname = parse_url($hostname, PHP_URL_HOST);
+    $analytics = "
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', '$google_analytics', '$hostname');
+        ga('send', 'pageview');
+
+      </script>
+    ";
+    return $analytics;
+  }
+}
 ?>
--- public/inc/header.php
+++ public/inc/header.php
@@ -13,17 +13,6 @@
     <link rel="stylesheet" href="style/screen.css">
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <script type='text/javascript' src='js/modernizr.min.js'></script>
-    <?php if($environment === 'production') { ?>
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', '<?php echo $google_analytics; ?>', '<?php echo $hostname; ?>');
-      ga('send', 'pageview');
-    </script>
-    <?php } ?>
+    <?php echo theAnalytics(); ?>
   </head>
   <body>
